aeb3568df79b05f92cbb295365d497e9f1be1136
djohnson135 <Dathanljohnson135@gmail.com>
2022-08-30 17:03:52 -0500

    Added ability to change brush color

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,8 @@ GLFWwindow *window;
 
 // Color structure. Can be used to define the brush and background color.
 struct color { float r, g, b; };
-color PIXEL_COLOR = { 1,0,0 };
+color BRUSH_COLOR = { 1,0,0 };
+color BACKGROUND_COLOR = { 0,0,0 };
 // A function clamping the input values to the lower and higher bounds
 #define CLAMP(in, low, high) ((in) < (low) ? (low) : ((in) > (high) ? (high) : in))
 
@@ -38,7 +39,7 @@ void SetFrameBufferPixel(int x, int y, struct color lc)
 void BrushDraw(int xpos, int ypos) {
 	for (int i = 0; i < STROKE_SIZE; i++) {
 		for (int j = 0; j < STROKE_SIZE; j++) {
-			SetFrameBufferPixel(CLAMP((xpos + STROKE_SIZE/2) - i, 0, WINDOW_WIDTH), CLAMP((ypos + STROKE_SIZE / 2)-j, 0, WINDOW_HEIGHT), PIXEL_COLOR);
+			SetFrameBufferPixel((xpos + STROKE_SIZE/2) - i, (ypos + STROKE_SIZE / 2)-j, BRUSH_COLOR);
 		}
 	}
 }
@@ -60,7 +61,6 @@ void CursorPositionCallback(GLFWwindow* lWindow, double xpos, double ypos)
 	int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
 	if (state == GLFW_PRESS)
 	{
-		//SetFrameBufferPixel((int)xpos, (int)ypos, PIXEL_COLOR);
 		BrushDraw((int)xpos, (int)ypos);
 		std::cout << "Mouse position is: x - " << xpos << ", y - " << ypos << std::endl;
 	}
@@ -75,6 +75,24 @@ void MouseCallback(GLFWwindow* lWindow, int button, int action, int mods)
 	}
 }
 
+color DigitToColor(int digit) {
+	int binary[32];
+	int iter = 0;
+	while (iter < 3) {
+		if (digit == 0) {
+			binary[iter++] = 0;
+		}
+		if (digit % 2 == 0) binary[iter++]=0;
+		else binary[iter++]=1;
+		digit /= 2;
+	}
+	color Color = {
+		(float)binary[2],
+		(float)binary[1],
+		(float)binary[0]
+	};
+	return Color;
+}
 // You can use "switch" or "if" to compare key to a specific character.
 // for example,
 // if (key == '0')
@@ -83,19 +101,52 @@ void MouseCallback(GLFWwindow* lWindow, int button, int action, int mods)
 // Keyboard callback function
 void CharacterCallback(GLFWwindow* lWindow, unsigned int key)
 {
-	switch ((char)key) {
-		case '+':
-			STROKE_SIZE = CLAMP(STROKE_SIZE*2, 1, 128);
-			break;
-		case '-':
-			STROKE_SIZE = CLAMP(STROKE_SIZE/2, 1, 128);
-			break;
+	if (std::isdigit(char(key))) {
+		int digit = char(key) - '0';
+		if (digit >= 0 && digit <= 7) {
+			BRUSH_COLOR = DigitToColor(digit);
+		}
 	}
-	
+	else {
+		switch ((char)key) {
+			case '+':
+				STROKE_SIZE = CLAMP(STROKE_SIZE * 2, 1, 128);
+				break;
+			case '-':
+				STROKE_SIZE = CLAMP(STROKE_SIZE / 2, 1, 128);
+				break;
+			case ')':
+				BACKGROUND_COLOR = DigitToColor(0);
+				break;
+			case '!':
+				BACKGROUND_COLOR = DigitToColor(1);
+				break;
+			case '@':
+				BACKGROUND_COLOR = DigitToColor(2);
+				break;
+			case '#':
+				BACKGROUND_COLOR = DigitToColor(3);
+				break;
+			case '$':
+				BACKGROUND_COLOR = DigitToColor(4);
+				break;
+			case '%':
+				BACKGROUND_COLOR = DigitToColor(5);
+				break;
+			case '^':
+				BACKGROUND_COLOR = DigitToColor(6);
+				break;
+			case '&':
+				BACKGROUND_COLOR = DigitToColor(7);
+				break;
+			default:
+				break;
+		}
+	}
 	std::cout << "Key " << (char)key << " is pressed." << std::endl;
 	std::cout << "Stroke Size " << STROKE_SIZE << std::endl;
-
 }
+
 
 void Init()
 {
